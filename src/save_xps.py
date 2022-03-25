343f11464d07b1e8d4e449e65a90edbc283b858e
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2022-03-25 14:02:53 +0800

    save_xps to save XPS data in two column format

---
+++ src/save_xps.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Program: Save XPS data in two column format
+Version: 20220325
+@author: Pranab Das (GitHub: @pranabdas)
+"""
+
+
+def save_xps(energy, intensity, filename=""):
+    import numpy as np
+    import datetime
+
+    if (filename == ""):
+        filename = "XPS_data_" + datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+
+    # check energy and intensity have same length
+    if (len(energy) != len(intensity)):
+        raise ValueError("Lengths of input arrays do not match")
+    else:
+        data = np.array([energy, intensity]).T
+        np.savetxt("filename", data)
