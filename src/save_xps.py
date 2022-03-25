7cb426041dea26240cce974a448a7ecf0210ee12
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2022-03-25 16:28:16 +0800

    Update save_xps.py

--- src/save_xps.py
+++ src/save_xps.py
@@ -12,11 +12,11 @@ def save_xps(energy, intensity, filename=""):
     import datetime
 
     if (filename == ""):
-        filename = "XPS_data_" + datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        filename = "XPS_data_" + datetime.datetime.now().strftime("%Y%m%d%H%M%S") + ".x_y"
 
     # check energy and intensity have same length
     if (len(energy) != len(intensity)):
         raise ValueError("Lengths of input arrays do not match")
     else:
         data = np.array([energy, intensity]).T
-        np.savetxt("filename", data)
+        np.savetxt(filename, data)
