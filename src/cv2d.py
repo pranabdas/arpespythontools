991f50ce7d597af33a1efe7d13341d43f2b0dc3a
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2022-12-04 01:09:26 +0800

    extend boundary while calculating convolution for laplacian and 2d-curvature

--- src/cv2d.py
+++ src/cv2d.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Program: 2D curvature of a spectra
-Version: 20210821
+Version: 20221204
 @author: Pranab Das (GitHub: @pranabdas)
 """
 
@@ -10,17 +10,12 @@ def cv2d(data, x, y, bw=5, c1=0.001, c2=0.001, w='default'):
     import numpy as np
     from astropy.convolution import convolve, Box2DKernel
     # https://docs.astropy.org/en/latest/api/astropy.convolution.Box2DKernel.html
+    # https://docs.astropy.org/en/latest/api/astropy.convolution.convolve.html
 
     if (w=='default'):
         w = data.shape[0]/data.shape[1]
 
-    data_smth = convolve(data, Box2DKernel(bw))
-
-    # crop the edges
-    bo = int(bw/2 + 1)
-    data_smth = data_smth[bo:-bo, bo:-bo]
-    x = x[bo:-bo]
-    y = y[bo:-bo]
+    data_smth = convolve(data, Box2DKernel(bw), boundary='extend')
 
     dE   = np.gradient(data_smth, axis=0)
     dT   = np.gradient(data_smth, axis=1)*w
@@ -32,11 +27,4 @@ def cv2d(data, x, y, bw=5, c1=0.001, c2=0.001, w='default'):
     cv2d = ((1 + c1*dE**2)*c2*d2T - 2*c1*c2*dE*dT*dEdT + (1 + c2*dT**2)*c1*d2E)/ \
                        (1 + c1*dE**2 + c2*dT**2)**1.5
 
-    # NaN wrapping for edges instead of cropping
-    # for ii in range(data.shape[0]):
-    #     for jj in range(data.shape[1]):
-    #         if (ii <= bo+1 or ii >= data.shape[0]-bo-1 or \
-    #             jj <= bo+1 or jj >= data.shape[1]-bo-1):
-    #             diff2[ii][jj] = np.nan
-
-    return cv2d, x, y
+    return cv2d
