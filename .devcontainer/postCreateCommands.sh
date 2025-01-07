167fc6a4f97a33ad3f30f3892e52bb8854fcbb86
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2025-01-07 16:39:48 +0800

    update devcontainer setup script

--- .devcontainer/postCreateCommands.sh
+++ .devcontainer/postCreateCommands.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 sudo apt update
 sudo apt install -y --no-install-recommends \
-    fonts-open-sans \
-    python3 \
-    python3-pip
-if [ -f requirements.txt ]; then
-    pip install -r requirements.txt
-fi
+  curl \
+  python3 \
+  python3-pip
+curl -sSL https://install.python-poetry.org | python3 -
+poetry install --all-extras
