e6993602dd813d8d6a5b9e736b2d235e3385944f
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2025-11-26 09:41:26 +0800

    add support for python 3.14 (#19)
    * add support for python 3.14
    
    * fix python version
    
    * bump deps
    
    * update docusaurus
    
    * regenerate package-lock.json
    
    * bump docusaurus x2
    
    * use build_docusaurus script for netlify
    
    * test latest astropy v7.2.0rc2
    
    * switch to astropy v7.2 stable release

--- docs/getting-started.md
+++ docs/getting-started.md
@@ -5,7 +5,7 @@ keywords: ["arpes", "data analysis", "data visualization", "python", "matplotlib
 ---
 ### Installation
 
-We will need [Python 3](https://www.python.org/) (version `3.10` - `3.13`)
+We will need [Python 3](https://www.python.org/) (version `3.10` - `3.14`)
 installed in the system. You can check the Python version in your system, using
 `python --version` in a terminal application. We can install latest stable
 release of **arpespytontools** from PyPI:
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,11 +10,15 @@ const config = {
   url: site_url,
   baseUrl: "/arpespythontools/", // must have a trailing "/"
   onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.png",
   organizationName: "pranabdas", // Usually your GitHub org/user name.
   projectName: "arpespythontools", // Usually your repo name.
   titleDelimiter: "•",
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: "warn",
+    },
+  },
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
@@ -183,7 +187,7 @@ const config = {
 
   stylesheets: [
     {
-      href: "https://pranab.pages.dev/assets/fonts/katex/0.16.22/katex.min.css",
+      href: "https://pranab.pages.dev/assets/fonts/katex/0.16.25/katex.min.css",
       type: "text/css",
     },
   ],
